2240fcbfb7cb93fe525c89de69fcb9d3ee154e43
Chris Keller <chris@commercialprogression.com>
2017-07-19 12:56:12 -0400

    Updated preview component

--- src/js/cms-preview-templates/homepage.js
+++ src/js/cms-preview-templates/homepage.js
@@ -10,23 +10,19 @@ export default class HomepagePreview extends React.Component {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
 
-    return <div>
-      <div className="pv5 pv6-l ph3 bg-center cover" style={{
-        backgroundImage: image && `url(${image})`
-      }}>
-        <div className="mw7 center ph3">
-          <div className="db mb3">
-            <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
-                { entry.getIn(["data", "hero", "headline"]) }
-              </h1>
-            </div>
-            <div className="mw7 relative bg-fix-primary">
-              <p className="b f4 di lh-title mb3 white mw6 bg-primary">
-                { entry.getIn(["data", "hero", "subtitle"]) }
-              </p>
-            </div>
-          </div>
+    return <div className="pv5 pv6-l ph3 bg-center cover" style={{
+      backgroundImage: image && `url(${image})`
+    }}>
+      <div className="mw7 center ph3">
+        <div className="db mb3">
+          <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+            { entry.getIn(["data", "hero", "headline"]) }
+          </h1>
+        </div>
+        <div className="mw7 relative bg-fix-primary">
+          <p className="b f4 di lh-title mb3 white mw6 bg-primary">
+            { entry.getIn(["data", "hero", "subtitle"]) }
+          </p>
         </div>
       </div>
     </div>;
